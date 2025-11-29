69758f72f809140694214e2afedf9c8de5ed7210
shabanovacpp <irinamay33@gmail.com>
2025-11-29 22:16:47 +0300

    no message

--- prn_generator.srcs/sources_1/new/tb.sv
+++ prn_generator.srcs/sources_1/new/tb.sv
@@ -3,19 +3,33 @@
 
 module tb;
 
+    parameter CODE_LENGTH_GLN = 511;
+    parameter CODE_LENGTH_GLN   = 511;
+    parameter CODE_LENGTH_GPS   = 1023;
+    parameter SR_LENGTH     = 10;   //shift register's length 
+
     logic clk;
     logic btn;
+    logic btn_rst;
     logic [7:0] seg;
     logic [3:0] act_seg;
-    logic led;
+    logic [2:0] led;
     logic led_done;
     logic led_rst;
     logic clk_btn;  
-    logic [4096-1:0] prn_test;  
     logic [SR_LENGTH-1:0] s_reg;
-    
-    top DUT (.clk(clk), .BTN(btn), .act_seg(act_seg), .LED(led), .LED_DONE(led_done), .LED_RST(led_rst), .seg(seg), .clk_btn(clk_btn),
-    .prn_test(prn_test), .s_reg(s_reg));
+    logic [CODE_LENGTH_GLN-1:0] prn_test_gln, 
+    logic [CODE_LENGTH_GPS-1:0] prn_test_gps1,
+    logic [CODE_LENGTH_GPS-1:0] prn_test_gps2,
+    logic [SR_LENGTH-1:0] s_reg_gln,
+    logic [SR_LENGTH-1:0] s_reg_gps_g1, 
+    logic [SR_LENGTH-1:0] s_reg_gps_g2
+
+
+    top DUT (.clk(clk), .BTN(btn), .BTN_RST(btn_rst), .act_seg(act_seg), .LED(led), 
+             .LED_RST(led_rst), .seg(seg), .tx(tx), .tx_busy(tx_busy), .clk_btn(clk_btn),
+             .prn_test_gln(prn_test_gln), .prn_test_gps1(prn_test_gps1), .prn_test_gps2(prn_test_gps2),
+             .s_reg_gln(s_reg_gln), .s_reg_gps_g1(s_reg_gps_g1), .s_reg_gps_g2(s_reg_gps_g2));
     
     initial begin
          
